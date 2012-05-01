719495a25c7c4778eb5baf6b4605f02647b55e67
Rashaad Ramdeen <rashaad.ramdeen@gmail.com>
2012-05-01 17:22:52 -0400

    removed garbage

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php 
-  $second = mktime(0,0,0,1,1,2013);
-  $first = mktime(0,0,0,22,3,2012); 
-  $difference = $first - $second;
-  echo ' hours, '; 
-  echo floor(floor($difference / 60) / 24); 
-?>
